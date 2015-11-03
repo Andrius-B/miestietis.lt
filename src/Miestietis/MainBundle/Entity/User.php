1e32fb78aad792e3b5fa750a052216f9d80a69dc
einorler <marc.mantas@gmail.com>
2015-11-03 11:22:45 +0200

    added a login

---
+++ src/Miestietis/MainBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+// src/Miestietis/MainBundle/Entity/User.php
+
+namespace Miestietis\MainBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
