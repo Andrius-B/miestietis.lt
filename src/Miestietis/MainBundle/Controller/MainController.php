03506401bf27354d75c3ead698f7a1feaed294bc
einorler <marc.mantas@gmail.com>
2015-11-04 14:28:54 +0200

    Made problema class, started implementing dynamic problem list

--- src/Miestietis/MainBundle/Controller/MainController.php
+++ src/Miestietis/MainBundle/Controller/MainController.php
@@ -6,10 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
+
+
     public function indexAction()
     {
-        return $this->render('MiestietisMainBundle:Main:index.html.twig', array(
+        $em = $this->getDoctrine()->getManager();
+        $problems = [];
+
+        for($i = 1; $i<4; $i++) {
+            $problems[] = $this->getDoctrine()
+                ->getRepository('MiestietisMainBundle:Problema')
+                ->find($i);
+        }
+
+        return $this->render('MiestietisMainBundle:Main:index.html.twig', array('problemos' => $problemos
                 // ...
-            ));    }
+            ));
+    }
 
 }
---
+++ src/Miestietis/MainBundle/Entity/Problema.php
@@ -0,0 +1,197 @@
+<?php
+// src/Miestietis/MainBundle/Entity/Product.php
+namespace Miestietis\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="problema")
+ */
+class Problema
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $user_id;
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $date;
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $description;
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    protected $votes;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $picture;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return Problema
+     */
+    public function setUserId($userId)
+    {
+        $this->user_id = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Problema
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Problema
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Problema
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set votes
+     *
+     * @param integer $votes
+     *
+     * @return Problema
+     */
+    public function setVotes($votes)
+    {
+        $this->votes = $votes;
+
+        return $this;
+    }
+
+    /**
+     * Get votes
+     *
+     * @return integer
+     */
+    public function getVotes()
+    {
+        return $this->votes;
+    }
+
+    /**
+     * Set picture
+     *
+     * @param string $picture
+     *
+     * @return Problema
+     */
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * Get picture
+     *
+     * @return string
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+}
