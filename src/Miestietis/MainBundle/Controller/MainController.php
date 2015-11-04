bfbdef7c435524788d43e2ae626890f3d35a1e96
Marius Virbicianskas <dblboy@gmail.com>
2015-11-04 17:09:34 +0200

    modified problem/initiative item frontend, separated banner and item sections into separate templates (hardcoded version), fixed trailing slash problem where /app_dev.php/ caused error in loading a banner image bg, updated fontawesome library

--- src/Miestietis/MainBundle/Controller/MainController.php
+++ src/Miestietis/MainBundle/Controller/MainController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
+
     public function indexAction()
     {
+
         return $this->render('MiestietisMainBundle:Main:index.html.twig', array(
-                // ...
+                'name' => "Marius"
             ));    }
 
 }
