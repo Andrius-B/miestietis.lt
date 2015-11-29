5745c3568fec0dc3e504c8db62a82fd35ecee589
Marius Virbicianskas <dblboy@gmail.com>
2015-11-29 20:55:58 +0200

    added comments for clarity; added functionality after add problem is successful empties input values and closes modal; added requireLogin everywhere where needs be;

--- web/public/js/custom.js
+++ web/public/js/custom.js
@@ -1,3 +1,4 @@
+// i have no idea what these do | Marius
 $(document).on('change', '.btn-file :file', function() {
     var input = $(this),
         numFiles = input.get(0).files ? input.get(0).files.length : 1,
@@ -6,6 +7,8 @@ $(document).on('change', '.btn-file :file', function() {
 });
 
 $(document).ready( function() {
+
+    // i have no idea what these do | Marius
     $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
 
         var input = $(this).parents('.input-group').find(':text'),
@@ -19,6 +22,9 @@ $(document).ready( function() {
 
     });
 
+    // -------------------------------------------------
+    // Ajax request to add problem
+
     var addProblem = $('#newProblemAjaxForm');
     $('#newProblem').on('click', addProblem, function(e){
         e.preventDefault();
@@ -27,6 +33,7 @@ $(document).ready( function() {
             var name =$('#itemName').val();
             var description = $('#itemDescription').val();
             var url = $('#controlerURL').attr('url');
+            var allInputs = $('#add .form-control');
             // final check if everything is
             $('#loading-img').show();
 
@@ -48,18 +55,25 @@ $(document).ready( function() {
                     $('#loading-img').hide();
                     console.log(data);
                     $("#imgdisplay").html("<img src='../images/problems"+data.picture+"'style='width: 150px'>");
-
-
                 },
                 error: function(XMLHttpRequest, textStatus, errorThrown)
                 {
                     alert('Error : ' + errorThrown);
+                },
+                complete: function() {
+                    $('.modal').modal('hide');
+                    allInputs.val('');
+                    location.reload(); // VERY VERY BAD PRACTICE reloads whole page, need some sort of handler maybe in backend, if mysql db is updated update the view
+                    $("#list").click();
                 }
             });
         }else{
-            alert('Norėdami paskelbti miesto problemą prisijunkite'); // custom modal or message line
+            requireLogin(false);
+            $('#newProblem').after('<span>Norėdami sukurti problemą turite prisijungti.</span>');
         }
     });
+    // End of add problem
+    //------------------------------------------------------------
 
 
     // -------------------------------------------------
@@ -108,41 +122,13 @@ $(document).ready( function() {
             });
         }
     });
-
-    function filters() {
-        if ($('.table-like').length>0) {
-            $('.table-like').fadeIn('slow');
-            $('#profile-more').on('shown.bs.modal', function() {
-                var $cont = $('.table-like').isotope({
-                    itemSelector: '.table-like__item',
-                    layoutMode: 'vertical',
-                    transitionDuration: '0.6s',
-                    filter: "*"
-                });
-                $('.filters-history').on( 'click', 'ul.nav-hist li a', function() {
-                    var filterValue = $(this).attr('data-filter');
-                    $(".filters-history").find("li.active").removeClass("active");
-                    $(this).parent().addClass("active");
-                    $cont.isotope({ filter: filterValue });
-                    return false;
-                });
-            });
-        }
-    }
-
-    function closeAndScroll(modal, target) {
-        $(modal).on('hidden.bs.modal', function() {
-            $('html, body').animate({
-                scrollTop: $(target).offset().top-151
-            }, 1000);
-        })
-    }
-
     // End of ajax load history
     //------------------------------------------------------------
 
 
-    //handle initiative form
+    // -------------------------------------------------
+    // Add initiative
+
     $('.openInitiativeModal').on('click', function(e){
         //hide opened problem modal
         $('.modal').modal('hide');
@@ -186,12 +172,17 @@ $(document).ready( function() {
                 alert('Visi laukai turi būti užpildyti');
             }
         } else {
-            alert('Norėdami paskelti miesto problemą prisijunkite');
+            requireLogin(false);
+            $('#submitButton').after('<span>Norėdami sukurti iniciatyvą turite prisijungti.</span>');
         }
     });
-    /*
-     need to add a check if user has already voted for this problem
-     */
+    // End of add initiative
+    //------------------------------------------------------------
+
+
+    // -------------------------------------------------
+    // Handle upvoting
+
     $('.incVote').on('click',function(){ //increment vote
         var item = $(this).children('i');
         var itemDisable = $(this);
@@ -218,20 +209,55 @@ $(document).ready( function() {
                 }
             });
         } else {
-            $('.modal').modal('hide');
-            $('#profileLi > a').css('animation', 'bounceIn 1s')
-                               .css('animation-iteration-count', 'infinite');
-
-            $(document).on('scroll shown.bs.modal', function() {
-                    $('#profileLi > a').css('animation-iteration-count', '1');
-                }
-            );
-            $('.isotope-item').mouseleave(function() {
-                $('#profileLi > a').css('animation-iteration-count', '1');
-            });
-
+            requireLogin(true);
         }
     });
+    // End of upvote
+    //------------------------------------------------------------
 
 
+    function filters() {
+        if ($('.table-like').length>0) {
+            $('.table-like').fadeIn('slow');
+            $('#profile-more').on('shown.bs.modal', function() {
+                var $cont = $('.table-like').isotope({
+                    itemSelector: '.table-like__item',
+                    layoutMode: 'vertical',
+                    transitionDuration: '0.6s',
+                    filter: "*"
+                });
+                $('.filters-history').on( 'click', 'ul.nav-hist li a', function() {
+                    var filterValue = $(this).attr('data-filter');
+                    $(".filters-history").find("li.active").removeClass("active");
+                    $(this).parent().addClass("active");
+                    $cont.isotope({ filter: filterValue });
+                    return false;
+                });
+            });
+        }
+    }
+
+    function requireLogin(hideModal) {
+        if(hideModal) {
+            $('.modal').modal('hide');
+        }
+        $('#profileLi > a').css('animation', 'bounceIn 1s')
+            .css('animation-iteration-count', 'infinite');
+
+        $(document).on('scroll shown.bs.modal', function() {
+                $('#profileLi > a').css('animation-iteration-count', '1');
+            }
+        );
+        $('.isotope-item').mouseleave(function() {
+            $('#profileLi > a').css('animation-iteration-count', '1');
+        });
+    }
+
+    function closeAndScroll(modal, target) {
+        $(modal).on('hidden.bs.modal', function() {
+            $('html, body').animate({
+                scrollTop: $(target).offset().top-151
+            }, 1000);
+        })
+    }
 });
