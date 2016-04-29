2f5f4d4d4c512a8c80046a0cb1e39ef254a0ec1a
Mantas <marc.mantas@gmail.com>
2016-04-29 21:20:16 +0300

    added initiativeController.php

---
+++ src/Miestietis/MainBundle/Controller/InitiativeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Miestietis\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class InitiativeController extends Controller
+{
+    /**
+     * @param string $initiative
+     */
+    public function displayAction($initiative)
+    {
+
+    }
+}
